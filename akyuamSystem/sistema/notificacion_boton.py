d85269a918714818907e801250de6b5eac25acaf
Juan Torres <juanestuardoje@gmail.com>
2024-10-27 18:30:20 -0600

    cambio boton

--- akyuamSystem/sistema/notificacion_boton.py
+++ akyuamSystem/sistema/notificacion_boton.py
@@ -19,5 +19,5 @@ def enviar_mensaje(nombre,apellido,direccion, telefono):
     message = client.messages.create(
         body=f"{nombre} {apellido} necesita ayuda, se encuentra en riesgo, su dirección es {direccion}, y su número de telefono es {telefono}",
         from_="+15306658627",
-        to="+50236313786",
+        to="+50231819847",
     )
